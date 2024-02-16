f59bcc1fa8163a7b4c37ed209c297e74b89f41cc
FNS <fernandonicolasanchez@gmail.com>
2024-02-16 16:03:25 -0300

    Iniciando resolucion de ejercicios

---
+++ Parciales_resueltos/parcial1_2023.cpp
@@ -0,0 +1,26 @@
+/**
+ * Ejercicios de repaso 2º Parcial 2023
+ * Author: FNS
+ * Descripción: Consignas en parcial1_2023.pdf 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+//Prototipos
+
+
+
+//Implementación de funciones
+
+
+
+
+//Prueba de funciones
+
+int main(){
+
+    
+    return 0;
+}
