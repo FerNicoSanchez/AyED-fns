6ebdee44df550752105acd1a4b285098ef1cf737
FNS <fernandonicolasanchez@gmail.com>
2024-02-16 22:14:53 -0300

    ej_1 OK

--- Parciales_resueltos/parcial1_2023.cpp
+++ Parciales_resueltos/parcial1_2023.cpp
@@ -10,17 +10,41 @@ using namespace std;
 
 //Prototipos
 
+void unionVectores(int A[],int B[], int C[]);
 
 
 //Implementación de funciones
 
-
+void unionVectores(int A[],int B[],int C[]){        //Ejercicio 1
+    int i;
+    for( i = 0 ; i<10 ; i++){
+        C[i] = A[i];
+    }
+    for (int b = 0; b < 10 ; b++ , i++){
+        C[i] = B[b];
+    }
+}
 
 
 //Prueba de funciones
 
 int main(){
 
-    
+    int A[10];
+    int B[10];
+    int C[20];
+
+    for(int i = 0 ; i<10 ; i++){
+        A[i] = i*10;
+    }
+    for(int i = 0 ; i<10 ; i++){
+        B[i] = 7;
+    }
+    unionVectores(A,B,C);
+
+    for( int i = 0; i<20 ; i++){
+        cout << i+1 <<"º dato: " << C[i] << endl; 
+    }
+
     return 0;
 }
